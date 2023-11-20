2f2a55f3fd01e19ff30f3d208113f4c4006bf4a5
Mireli Araujo <mirellypollocks@gmail.com>
2023-11-20 08:08:38 -0300

    criando seções assista do seu jeito, dispositivos disponíveis e Faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
     
     for (let i = 0; i < buttons.length; i++) {
@@ -12,7 +13,18 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreFechaResposta);
+    }
 })
+
+function abreFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -22,7 +34,7 @@ function removeBotaoAtivo() {
 
     }
 }
- 
+
 function escondeAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
